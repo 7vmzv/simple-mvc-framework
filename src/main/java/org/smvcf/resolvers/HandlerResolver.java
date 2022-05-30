d2d846527c2c7afc03e4f63a3babfe309956ae6b
7vmzv <rbatihamza@hotmail.com>
2022-05-30 16:42:30 +0200

    implemted and tested getHandler methode to resolve which controller to instantiate

---
+++ src/main/java/org/smvcf/resolvers/HandlerResolver.java
@@ -0,0 +1,25 @@
+package org.smvcf.resolvers;
+
+public class HandlerResolver {
+    private String url;
+    private String controller;
+    private String methode;
+
+    public String getHandler(){
+        if(url.equals("")) {
+            return "ErrorController";
+        }
+        splitTheUrlAndResolveControllerAndMethod();
+        return controller.substring(0, 1).toUpperCase() + controller.substring(1) + "Controller";
+    }
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+
+    private void splitTheUrlAndResolveControllerAndMethod(){
+        String[] controllerAndMethode =  url.split("/");
+        controller = controllerAndMethode[0];
+        methode = controllerAndMethode[1];
+    }
+}
---
+++ src/test/java/org/smvcf/resolvers/HandlerResolverTest.java
@@ -0,0 +1,35 @@
+package org.smvcf.resolvers;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * ProperUrl definition : /controllerName/methodeName
+ */
+public class HandlerResolverTest {
+    //Arrange
+    HandlerResolver handlerResolver = new HandlerResolver();
+
+    @Test
+    public void should_return_ErrorController_when_given_empty_string(){
+        //Act
+        handlerResolver.setUrl("");
+        String actual = handlerResolver.getHandler();
+
+        //Assert
+        assertEquals("ErrorController", actual);
+    }
+
+    @Test
+    public void should_return_proper_controller_name_when_given_proper_url(){
+        //Act
+        handlerResolver.setUrl("controllerName/methodeName");
+        String actual = handlerResolver.getHandler();
+
+        //Assert
+        assertEquals("ControllerNameController", actual);
+    }
+
+}
