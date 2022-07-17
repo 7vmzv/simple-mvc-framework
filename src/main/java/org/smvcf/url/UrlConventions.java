48943513963a49f9c0615e6c2324d64f5f522a0c
7vmzv <rbatihamza@hotmail.com>
2022-07-17 17:19:50 +0200

    add url format convention

--- src/main/java/org/smvcf/url/UrlConventions.java
+++ src/main/java/org/smvcf/url/UrlConventions.java
@@ -3,4 +3,6 @@ package org.smvcf.url;
 public class UrlConventions {
     public static String CONTROLLER_NAME_FORMAT = "[a-zA-Z][a-zA-Z0-9_]*";
     public static String METHODE_NAME_FORMAT = "[a-z][a-zA-Z0-9_]*";
+    public static String URL_CONVENTION = "/" + CONTROLLER_NAME_FORMAT + "/" + METHODE_NAME_FORMAT;
+
 }
