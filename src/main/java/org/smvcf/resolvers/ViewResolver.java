84c353546a224e403060d43efe02de7e174ebb62
7vmzv <rbatihamza@hotmail.com>
2022-07-17 16:37:23 +0200

    refactor ViewResolver

--- src/main/java/org/smvcf/resolvers/ViewResolver.java
+++ src/main/java/org/smvcf/resolvers/ViewResolver.java
@@ -2,18 +2,19 @@ package org.smvcf.resolvers;
 
 
 public class ViewResolver {
+    private String index = "index";
     private String prefix;
     private String suffix;
 
     public String getViewPage(String viewPage){
         if(viewPage == null || viewPage.equals("")) {
-            return "";
+            return this.prefix + index + this.suffix;
         }
-        return this.prefix + "/" + viewPage + this.suffix;
+        return this.prefix + viewPage + this.suffix;
     }
 
     public ViewResolver() {
-        this.prefix = "/WEB-INF/view";
+        this.prefix = "/WEB-INF/view/";
         this.suffix = ".jsp";
     }
 }
