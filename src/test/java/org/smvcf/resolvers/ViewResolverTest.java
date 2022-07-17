0d54269b0313235b8684781bb2473ed1b9b718dc
7vmzv <rbatihamza@hotmail.com>
2022-07-17 16:39:17 +0200

    fix expected result for empty and null string values to '/WEB-INF/view/index.jsp'

--- src/test/java/org/smvcf/resolvers/ViewResolverTest.java
+++ src/test/java/org/smvcf/resolvers/ViewResolverTest.java
@@ -14,20 +14,20 @@ public class ViewResolverTest {
         String actual = viewResolver.getViewPage(null);
 
         //assert
-        assertEquals("", actual);
+        assertEquals("/WEB-INF/view/index.jsp", actual);
     }
 
     @Test
-    public void should_return_empty_string_when_given_empty_one(){
+    public void should_return_index_when_given_empty_one(){
         //act
         String actual = viewResolver.getViewPage("");
 
         //assert
-        assertEquals("", actual);
+        assertEquals("/WEB-INF/view/index.jsp", actual);
     }
 
     @Test
-    public void should_return_prefix_concatenated_with_input_concatenated_with_suffix_when_given_non_empty_or_null_string(){
+    public void should_return_prefix_concatenated_with_input_concatenated_with_suffix_when_given_non_empty_or_not_null_string(){
         //act
         String actual = viewResolver.getViewPage("hamza");
 
