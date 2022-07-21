51096d63f549519c8d05428e3ac2f6836032583a
7vmzv <rbatihamza@hotmail.com>
2022-07-21 17:30:59 +0000

    fixed a test

--- src/test/java/org/smvcf/resolvers/HandlerResolverTest.java
+++ src/test/java/org/smvcf/resolvers/HandlerResolverTest.java
@@ -17,13 +17,15 @@ public class HandlerResolverTest {
     HandlerResolver handlerResolver = new HandlerResolver();
 
     @Test
-    public void should_return_ErrorController_when_given_empty_string(){
-        //Act
+    public void should_throw_when_given_empty_string(){
+        
         handlerResolver.setUrl("");
-        String actual = handlerResolver.getHandler();
+        
 
-        //Assert
-        assertEquals("ErrorController", actual);
+        //Act //Assert
+        assertThrows(InvalidControllerNameException.class, () -> {
+            String actual = handlerResolver.getHandler();
+        });
     }
 
     @Test
